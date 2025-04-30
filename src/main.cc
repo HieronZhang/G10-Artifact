4e058161d9b58e3290e1c406cd1347ae0112f761
haoyoung <zhang402@illinois.edu>
2025-04-30 09:20:08 +0000

    fix

--- src/main.cc
+++ src/main.cc
@@ -579,7 +579,7 @@ int main(int argc, char *argv[]) {
 
     migration_plan_output_file = output_folder_name + "/" + "migration_plan.txt";
 
-    // migration_plan_output.open(migration_plan_output_file);
+    migration_plan_output.open(migration_plan_output_file);
 
     // return 0;
 
