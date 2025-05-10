8df0ffc7693e46ed655d1f3249be1336f14cb82c
haoyoung <zhang402@illinois.edu>
2025-05-10 20:25:51 +0000

    update profiles of 70b_16_1k_4ubs and all plans for it and "max" using 1485GB CPU memory

--- src/configs/create_max_configs.py
+++ src/configs/create_max_configs.py
@@ -2,18 +2,16 @@
 import os
 import sys
 
-settings = ["8b_16_2k_4ubs", "8b_32_2k_4ubs", "8b_64_1k_8ubs", "8b_64_2k_4ubs", "8b_64_4k_2ubs", "8b_128_2k_4ubs", "70b_8_2k_1ubs", "70b_16_1k_1ubs", "70b_16_2k_1ubs", "70b_16_3k_1ubs", "70b_16_4k_1ubs", "70b_32_2k_1ubs", "70b_64_2k_1ubs"
-            , "granite_8b_bs16_seq4k_ubs4", "granite_8b_bs32_seq4k_ubs4", "granite_8b_bs64_seq4k_ubs4", "granite_8b_bs64_seq2k_ubs8", "granite_8b_bs64_seq8k_ubs2", "granite_8b_bs128_seq4k_ubs4"
-            , "70b_8_2k_2ubs", "70b_16_1k_2ubs", "70b_16_2k_2ubs", "70b_16_3k_2ubs", "70b_16_4k_2ubs", "70b_32_2k_2ubs", "70b_32_2k_4ubs", "70b_16_1k_4ubs"]
+settings = ["70b_8_2k_2ubs", "70b_16_1k_2ubs", "70b_16_2k_2ubs", "70b_16_3k_2ubs", "70b_16_4k_2ubs", "70b_32_2k_2ubs", "70b_32_2k_4ubs", "70b_16_1k_4ubs"]
 
-speedups = ["1", "2", "4", "4", "4", "10", "10", "20", "20", "20", "20", "40", "40", "5", "10", "10", "10", "10", "10",  "5", "10", "10", "10", "10", "20", "20", "20"]
+speedups = [ "5", "10", "10", "10", "10", "20", "20", "20"]
 
 ranks = ["0", "1"]
 
 gpu_sizes = ["82", "80", "78", "76"]
 
-cpu_sizes = [["130", "130", "130", "130", "130", "130", "422", "422","422","422","422","422","422","137","137","137","137","137","137", "422","422","422","422","422","422", "422", "422"],
-             ["40", "40", "40", "40", "40", "40",  "348", "348","348","348","348","348","348","65","65","65","65","65","65", "348","348","348","348","348","348", "348", "348"]]
+cpu_sizes = [["814","814","814","814","814","814", "814", "814"],
+             ["671","671","671","671","671","671", "671", "671"]]
 
 for i in range(len(settings)):
     # create the directories first
@@ -21,16 +19,12 @@ for i in range(len(settings)):
     speedup = speedups[i]
     cpu_usage = ""
     for rank in ranks:
-        for j in range(2):
-            if j == 0:
-                if rank == "0":
-                    cpu_size = cpu_sizes[0][i]
-                else:
-                    cpu_size = cpu_sizes[1][i]
-                cpu_usage = "mixed"
+
+            if rank == "0":
+                cpu_size = cpu_sizes[0][i]
             else:
-                cpu_size = "0"
-                cpu_usage = "ssdonly"
+                cpu_size = cpu_sizes[1][i]
+            cpu_usage = "max"
             for gpu_size in gpu_sizes:
                 output_dir = "torchtitan_" + rank + "_" + model_i + "_gpu" + gpu_size + "_" + cpu_usage
                 directory_name = "data_" + model_i
